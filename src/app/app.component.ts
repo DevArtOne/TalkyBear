159198df379308984b6b004efea38cd1860021ef
DevArtOne <104894517+DevArtOne@users.noreply.github.com>
2022-05-04 12:03:44 +0300

    Test
    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { NgwWowService } from 'ngx-wow';
+import { filter, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Talky Bear';
+
+
+
+
+  private wowSubscription: Subscription | undefined;
+
+  constructor(private router: Router, private wowService: NgwWowService) {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      // Reload WoW animations when done navigating to page,
+      // but you are free to call it whenever/wherever you like
+      this.wowService.init();
+    });
+
+  }
+
+  ngOnInit() {
+    // you can subscribe to WOW observable to react when an element is revealed
+    this.wowSubscription = this.wowService.itemRevealed$.subscribe(
+      (item: HTMLElement) => {
+        // do whatever you want with revealed element
+      });
+  }
+
+  ngOnDestroy() {
+    // unsubscribe (if necessary) to WOW observable to prevent memory leaks
+    this.wowSubscription?.unsubscribe();
+  }
+}
